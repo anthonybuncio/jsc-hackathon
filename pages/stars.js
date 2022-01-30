1e342b91f38c368ede91c4830983ef83d8ddc94f
Anthony <anthonyb.ft@gmail.com>
2022-01-30 06:53:01 -0600

    Interviews addedgit add ...

--- pages/stars.js
+++ pages/stars.js
@@ -1,9 +1,5 @@
 import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
 
 export default function Home() {
   return (
@@ -11,19 +7,41 @@ export default function Home() {
       <Header />
 
       <main className={styles.main}>
-        <>
-          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-          </Swiper>
-        </>
+        <div className={styles.starsContainer}>
+          <div className={styles.starsJob}>
+            <h1 className={styles.starsTitle}>
+              Meet Alex, a Data Scientist and Machine Learning Practitioner with
+              NASA's Earth Science and Remote Sensing Unit.
+            </h1>
+            <div className={styles.videoContainer}>
+              <video
+                src={require('../public/rocket-interview.mp4')}
+                autoPlay
+                controls
+              />
+            </div>
+          </div>
+          <div className={styles.starsJob}>
+            <h1 className={styles.starsTitle}>
+              Meet Robert, a Habitability Domain Lead and CO-Lead Center for
+              Design and Space architecture at NASA.
+            </h1>
+            <div className={styles.videoContainer}>
+              <video src={require('../public/planet-interview.mp4')} controls />
+            </div>
+          </div>
+          <div className={styles.starsJob}>
+            <h1 className={styles.starsTitle}>
+              Meet Nate, a Pointing Flight Controller at NASA.
+            </h1>
+            <div className={styles.videoContainer}>
+              <video
+                src={require('../public/wordmatch-interview.mp4')}
+                controls
+              />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
